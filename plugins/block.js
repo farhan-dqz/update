7f26ae530f776b4713efb61e08c1abf4ec437844
Farhan_Dqz <83164448+farhan-dqz@users.noreply.github.com>
2021-11-03 20:24:39 +0530

    Update block.js

--- plugins/block.js
+++ plugins/block.js
@@ -41,7 +41,7 @@ if (Config.LANG == 'EN') plk2 = '.block'
 if (Config.LANG == 'ML') plk = 'ക്ഷമിക്കണം PM ൽ ചാറ്റിംഗ് അനുവദനീയമല്ല'
 if (Config.LANG == 'ML') plk2 = '.block'
 Asena.addCommand({on: 'text', fromMe: false, onlyPm: true , deleteCommand: false}, (async (message, match) => {
-    if (pmblock_var == 'true' && message.jid !== Config.PM_ALLOWED) {
+    if (pmblock_var == 'true' && message.jid !== Config.BGMFILTER) {
         let regex1 = anything
         if (!regex1.test(message.message)) {
            await message.client.sendMessage(message.jid,plk, MessageType.text, {quoted: message.data })
